3bba85be3954968415eae8dbd1511ea2c41fb948
Soslan Aldatov <me@sptm.dev>
2023-03-13 02:28:44 +0400

    fix migration issues

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,19 +1,35 @@
 import "reflect-metadata";
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { DataSource } from "typeorm";
-import config from "./utils/config.js";
-import { Book } from "./entity/Book.js";
-import { User } from "./entity/User.js";
-import { Book1678198313268 } from "./migrations/1678198313268-Book.js";
 
-export const AppDataSource = new DataSource({
-  "type": "mongodb",
-  "url": config.MONGODB_URI,
-  "useNewUrlParser": true,
-  "logging": true,
-  "ssl": true,
-  "authSource": "admin",
-  "database": "books",
-  "entities": [Book, User],
-  "migrations": [Book1678198313268],
-  "subscribers": [],
+import config from "./utils/config.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+const MongoDataSource = new DataSource({
+  type: "mongodb",
+  url: config.MONGODB_URI,
+  useNewUrlParser: true,
+  logging: true,
+  database: 'books',
+  ssl: false,
+  entities: [ `${__dirname}/entity/*.ts`],
+  migrations: [ `${__dirname}/migrations/*.ts` ],
+  subscribers: [],
 });
+
+const PostgresDataSource = new DataSource({
+  type: 'postgres',
+  url: config.POSTGRES_URI,
+  logging: true,
+  ssl: false,
+  database: 'books',
+  entities: [ `${__dirname}/entity/*.ts`],
+  migrations: [ `${__dirname}/migrations/*.ts` ],
+  subscribers: [],
+});
+
+export { PostgresDataSource };
